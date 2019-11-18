3e83c9c0a39b9cda4ec74fcc2bae1588568066db
myxy99 <2399158611@qq.com>
2019-11-18 14:11:55 +0800

    model

---
+++ database/migrations/2019_11_18_141117_create_project_modules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_modules');
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-##api管理工具
+# api管理工具
+
+php artisan make:model Model/User -m
+php artisan make:model Model/Project -m
+php artisan make:model Model/Log -m
+php artisan make:model Model/Mutual -m
+php artisan make:model Model/Position -m
+php artisan make:model Model/Interface -m
+php artisan make:model Model/ProjectMember -m
+php artisan make:model Model/Annex -m
+php artisan make:model Model/Request -m
+php artisan make:model Model/Response -m
+php artisan make:model Model/ProjectModule -m
+php artisan make:model Model/FeedBack -m
