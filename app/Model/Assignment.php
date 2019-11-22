a1c1c157db5b8bffc6351eae87f12105356bf7c0
myxy99 <2399158611@qq.com>
2019-11-22 10:15:10 +0800

    assignments

---
+++ app/Model/Assignment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    //定义模型关联的数据表
+    protected $table = 'assignments';
+    //定义主键
+    protected $primaryKey = 'id';
+    //定义禁止操作时间
+    public $timestamps = false;
+}
---
+++ database/migrations/2019_11_22_100752_create_assignments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->increments('id')->comment('任务分配表主键id');//任务分配表主键id
+
+            $table->integer('module_id')->unsigned()->comment('外键project_modules表主键id,设置restrict');
+            $table->foreign('module_id')//外键project_modules表主键id,设置restrict
+                ->references('id')->on('project_modules');
+
+            $table->integer('user_id')->unsigned()->comment('外键user表主键id,设置restrict');
+            $table->foreign('user_id')//外键user表主键id,设置restrict
+                ->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
