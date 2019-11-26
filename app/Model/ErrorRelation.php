87816775ab326f566912ecef6ba8320cb84839f5
myxy99 <2399158611@qq.com>
2019-11-26 08:54:00 +0800

    error

---
+++ app/Model/ErrorRelation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ErrorRelation extends Model
+{
+    //定义模型关联的数据表
+    protected $table = 'error_relations';
+    //定义主键
+    protected $primaryKey = 'id';
+    //定义禁止操作时间
+    public $timestamps = false;
+}
--- database/migrations/2019_11_22_100752_create_assignments_table.php
+++ database/migrations/2019_11_22_100752_create_assignments_table.php
@@ -27,7 +27,6 @@ class CreateAssignmentsTable extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
