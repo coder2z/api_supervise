d582c00502d64e5574030d5363e273f9839c9d2e
Likecolors <44368039+Likecolors@users.noreply.github.com>
2019-12-12 14:45:20 +0800

    修改

--- app/Http/Requests/Message/FrontEndMsg.php
+++ app/Http/Requests/Message/FrontEndMsg.php
@@ -1,20 +1,12 @@
 <?php
 
-<<<<<<< HEAD:app/Http/Requests/Message/FrontEndMsg.php
-namespace App\Http\Message\Requests;
-=======
 namespace App\Http\Requests\ProjectAdmin;
->>>>>>> upstream/master:app/Http/Requests/ProjectAdmin/searchRequest.php
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-<<<<<<< HEAD:app/Http/Requests/Message/FrontEndMsg.php
-class FrontEndMsg extends FormRequest
-=======
 class searchRequest extends FormRequest
->>>>>>> upstream/master:app/Http/Requests/ProjectAdmin/searchRequest.php
 {
     /**
      * Determine if the user is authorized to make this request.
