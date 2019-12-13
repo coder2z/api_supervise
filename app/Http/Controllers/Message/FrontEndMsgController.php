95805217adb00225b91fb1c274a594cc76d16eb3
myxy99 <2399158611@qq.com>
2019-12-13 17:15:08 +0800

    check

--- app/Http/Controllers/Message/FrontEndMsgController.php
+++ app/Http/Controllers/Message/FrontEndMsgController.php
@@ -78,7 +78,7 @@ class FrontEndMsgController extends Controller
      * @return \Illuminate\Http\JsonResponse
      * @throws \Exception
      */
-    public function MyMessages(Request $request)
+    public function MyMessage(Request $request)
     {
         try {
             $data = FeedBack::getInfo_echo($request->id);
--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,7 @@ Route::namespace('Admin')->group(function () {
 });
 //
 //倪煜
-Route::get("/MyMessages", "Message\FrontEndMsgController@MyMessages");//获取反馈信息列表
+Route::get("/MyMessage", "Message\FrontEndMsgController@MyMessage");//获取反馈信息列表
 Route::get("/SendMail_All_Back", "Message\FrontEndMsgController@SendMail_All_Back");//前端增加所在项目反馈信息并发送邮件（项目全部后端）
 //qiu
 Route::namespace('Admin')->group(function () {
